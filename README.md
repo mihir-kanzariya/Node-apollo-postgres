7c3515afa09baebc77234fdcc9e6d503d0436f12
Mihir Kanzariya <kanzariyamihir@gmail.com>
2019-12-28 19:53:26 +0530

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+## Installation
+
+- `git clone`
+- `cd`
+- `touch .env`
+- `npm install`
+- fill out _.env file_
+- start PostgreSQL database
+- `npm start`
+- visit `http://localhost:8000` for GraphQL playground
---
+++ src/index.js
@@ -0,0 +1,119 @@
+import 'dotenv/config';
+import cors from 'cors';
+import morgan from 'morgan';
+import http from 'http';
+import jwt from 'jsonwebtoken';
+import DataLoader from 'dataloader';
+import express from 'express';
+import {
+  ApolloServer,
+  AuthenticationError,
+} from 'apollo-server-express';
+console.log(
+  'process.env.SECRET>>>>>>>>>>>>>>>>>>>>>>',
+  process.env.SECRET
+);
+import schema from './schema';
+import resolvers from './resolvers';
+import models, { sequelize } from './models';
+import loaders from './loaders';
+
+const app = express();
+
+app.use(cors());
+
+app.use(morgan('dev'));
+
+const getMe = async req => {
+  const token = req.headers['x-token'];
+
+  if (token) {
+    try {
+      return await jwt.verify(token, process.env.SECRET);
+    } catch (e) {
+      throw new AuthenticationError(
+        'Your session expired. Sign in again.'
+      );
+    }
+  }
+};
+
+const server = new ApolloServer({
+  introspection: true,
+  playground: true,
+  typeDefs: schema,
+  resolvers,
+  formatError: error => {
+    // remove the internal sequelize error message
+    // leave only the important validation error
+    const message = error.message
+      .replace('SequelizeValidationError: ', '')
+      .replace('Validation error: ', '');
+
+    return {
+      ...error,
+      message,
+    };
+  },
+  context: async ({ req, connection }) => {
+    if (connection) {
+      return {
+        models,
+        loaders: {
+          user: new DataLoader(keys =>
+            loaders.user.batchUsers(keys, models)
+          ),
+        },
+      };
+    }
+
+    if (req) {
+      const me = await getMe(req);
+
+      return {
+        models,
+        me,
+        secret: process.env.SECRET,
+        loaders: {
+          user: new DataLoader(keys =>
+            loaders.user.batchUsers(keys, models)
+          ),
+        },
+      };
+    }
+  },
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+const isTest = !!process.env.TEST_DATABASE;
+const isProduction = !!process.env.DATABASE_URL;
+const port = process.env.PORT || 8000;
+
+sequelize.sync({ force: isTest || isProduction }).then(async () => {
+  if (isTest || isProduction) {
+    createUsers(new Date());
+  }
+
+  httpServer.listen({ port }, () => {
+    console.log(`Apollo Server on http://localhost:${port}/graphql`);
+  });
+});
+
+const createUsers = async date => {
+  await models.User.create({
+    username: 'mihir12345',
+    email: 'hello@frnd.com',
+    password: 'mihir12345',
+    role: 'ADMIN',
+  });
+
+  await models.User.create({
+    username: 'asasasas',
+    email: 'hello@asasas.com',
+    password: 'ddavasasids',
+  });
+};
